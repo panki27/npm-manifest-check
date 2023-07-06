44690f927aa1cbd1fb308461c38fdf5c4bfd5d07
Harm Zeinstra <80104552+Harm-Nullix@users.noreply.github.com>
2023-07-06 16:01:51 +0200

    Update check_and_output_packages.sh

--- check_and_output_packages.sh
+++ check_and_output_packages.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 
 VERBOSE=0
-FORMAT=json
 OUTPUT=npm-manifest-check-results
 for arg in $@
 do
@@ -9,31 +8,20 @@ do
     -h)
       echo """
 ***
-Usage: check_and_output_packages.sh [<name>] [--format=json|html|text] [--verbose]
+Usage: check_and_output_packages.sh [<name>] [--verbose]
 ***
 """
       exit 0 ;;
     --verbose*) VERBOSE=1;;
-    --format*)
-      TMP_FORMAT=${arg:9}
-      case $TMP_FORMAT in
-        text) FORMAT=text;;
-        html) FORMAT=html;;
-        json) FORMAT=json;;
-      esac;;
     *)
       OUTPUT=$arg;;
   esac
 done
-OUTPUT="${OUTPUT}.${FORMAT}"
 
-RESULTS=""
-if [ $FORMAT == "json" ]; then
-  RESULTS="["
-fi
-if [ $FORMAT == "html" ]; then
-  RESULTS="<html><head><style>*{font-family:Arial, sans-serif}table{border-collapse:collapse}td,th{padding:8px}.good{background-color:#00531f;color:rgb(132, 251, 151)}.bad{background-color:#930100;color:rgb(255, 218, 212)}tr{border:1px solid #8b9389}</style></head><body><table><thead><tr><th>Package</th><th>Output</th></tr></thead><tbody>"
-fi
+RESULTS_ERRORS=""
+RESULTS_TEXT=""
+RESULTS_HTML="<html><head><style>*{font-family:Arial, sans-serif}table{border-collapse:collapse}td,th{padding:8px}.good{background-color:#00531f;color:rgb(132, 251, 151)}.bad{background-color:#930100;color:rgb(255, 218, 212)}tr{border:1px solid #8b9389}</style></head><body><table><thead><tr><th>Package</th><th>Output</th></tr></thead><tbody>"
+RESULTS_JSON="["
 
 while IFS= read -r pkg; do
   if [ $VERBOSE == 1 ]; then
@@ -45,48 +33,49 @@ while IFS= read -r pkg; do
     echo "${PKG_RESULT}"
   fi
 
-  case $FORMAT in
-    text)
-      CONCLUSION=" (ERROR)"
-      case $PKG_RESULT in
-        *"No mismatch detected for"*) CONCLUSION="";;
-      esac
-      RESULTS="${RESULTS}${pkg}${CONCLUSION}: ${PKG_RESULT}"$'\n\r'
-    ;;
-    html)
-      CLASSNAME=bad
-      case $PKG_RESULT in
-        *"No mismatch detected for"*) CLASSNAME=good;;
-      esac
-      RESULTS="${RESULTS}<tr class=\"${CLASSNAME}\"><td>${pkg}</td><td>${PKG_RESULT//\!/!<br>}</td></tr>"
-    ;;
-
-    json)
-      case $PKG_RESULT in
-          *"No mismatch detected for"*) RESULTS="${RESULTS}{\"success\":true,\"pkg\":\"${pkg}\",\"output\":\"${PKG_RESULT}\"},";;
-          *)
-            RESULTS="${RESULTS}{\"success\":false,\"pkg\":\"${pkg}\",\"output\":["
-
-            oIFS="$IFS"; IFS='!' # split on .
-            set -o noglob     # disable glob
-            set -- $PKG_RESULT"" # split+glob with glob disable
-            IFS="$oIFS"
-            for LINE in "$@"
-            do
-              RESULTS="${RESULTS}\"${LINE}\","
-            done
-            RESULTS="${RESULTS%,}]},"
-          ;;
-        esac
-  ;;
+# ERRORS
+  case $PKG_RESULT in
+    *"No mismatch detected for"*) RESULTS_ERRORS="${RESULTS_ERRORS}";;
+    *) RESULTS_ERRORS="${RESULTS_ERRORS}${pkg}"$'\n\r';;
   esac
+
+# TEXT
+  CONCLUSION=" (ERROR)"
+  case $PKG_RESULT in
+    *"No mismatch detected for"*) CONCLUSION="";;
+  esac
+  RESULTS_TEXT="${RESULTS_TEXT}${pkg}${CONCLUSION}: ${PKG_RESULT}"$'\n\r'
+
+# HTML
+  CLASSNAME=bad
+  case $PKG_RESULT in
+    *"No mismatch detected for"*) CLASSNAME=good;;
+  esac
+  RESULTS_HTML="${RESULTS_HTML}<tr class=\"${CLASSNAME}\"><td>${pkg}</td><td>${PKG_RESULT//\!/!<br>}</td></tr>"
+
+# JSON
+  case $PKG_RESULT in
+      *"No mismatch detected for"*) RESULTS_JSON="${RESULTS_JSON}{\"success\":true,\"pkg\":\"${pkg}\",\"output\":\"${PKG_RESULT}\"},";;
+      *)
+        RESULTS_JSON="${RESULTS_JSON}{\"success\":false,\"pkg\":\"${pkg}\",\"output\":["
+
+        oIFS="$IFS"; IFS='!' # split on .
+        set -o noglob     # disable glob
+        set -- $PKG_RESULT"" # split+glob with glob disable
+        IFS="$oIFS"
+        for LINE in "$@"
+        do
+          RESULTS_JSON="${RESULTS_JSON}\"${LINE}\","
+        done
+        RESULTS_JSON="${RESULTS_JSON%,}]},"
+      ;;
+    esac
 done < packages.list
 
-if [ $FORMAT == "json" ]; then
-  RESULTS="${RESULTS%,}]"
-fi
-if [ $FORMAT == "html" ]; then
-  RESULTS="${RESULTS}</tbody></table></body></html>"
-fi
+RESULTS_HTML="${RESULTS_HTML}</tbody></table></body></html>"
+RESULTS_JSON="${RESULTS_JSON%,}]"
 
-echo $RESULTS > $OUTPUT
+echo $RESULTS_ERRORS > "${OUTPUT}-errors.txt"
+echo $RESULTS_TEXT > "${OUTPUT}.txt"
+echo $RESULTS_HTML > "${OUTPUT}.html"
+echo $RESULTS_JSON > "${OUTPUT}.json"
