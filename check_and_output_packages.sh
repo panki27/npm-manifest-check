5fced4b4969501b146774fa4ec885a219f1fd5f7
Harm Zeinstra <80104552+Harm-Nullix@users.noreply.github.com>
2023-07-06 16:28:21 +0200

    Update check_and_output_packages.sh

--- check_and_output_packages.sh
+++ check_and_output_packages.sh
@@ -18,12 +18,17 @@ Usage: check_and_output_packages.sh [<name>] [--verbose]
   esac
 done
 
+RESULTS_META_TOTAL=0
+RESULTS_META_SUCCESS=0
+RESULTS_META_ERRORS=0
 RESULTS_ERRORS=""
 RESULTS_TEXT=""
-RESULTS_HTML="<html><head><style>*{font-family:Arial, sans-serif}table{border-collapse:collapse}td,th{padding:8px}.good{background-color:#00531f;color:rgb(132, 251, 151)}.bad{background-color:#930100;color:rgb(255, 218, 212)}tr{border:1px solid #8b9389}</style></head><body><table><thead><tr><th>Package</th><th>Output</th></tr></thead><tbody>"
+RESULTS_HTML="<html><head><style>*{font-family:Arial, sans-serif}table{border-collapse:collapse}td,th{padding:8px}.good{background-color:#00531f;color:rgb(132, 251, 151)}.bad{background-color:#930100;color:rgb(255, 218, 212)}tr{border:1px solid #8b9389}</style></head><body>"
+RESULTS_HTML_TABLE="<table><thead><tr><th>Package</th><th>Output</th></tr></thead><tbody>"
 RESULTS_JSON="["
 
 while IFS= read -r pkg; do
+  RESULTS_META_TOTAL="$((RESULTS_META_TOTAL+1))"
   if [ $VERBOSE == 1 ]; then
     echo "Running for ${pkg}"
   fi
@@ -35,8 +40,13 @@ while IFS= read -r pkg; do
 
 # ERRORS
   case $PKG_RESULT in
-    *"No mismatch detected for"*) RESULTS_ERRORS="${RESULTS_ERRORS}";;
-    *) RESULTS_ERRORS="${RESULTS_ERRORS}${pkg}"$'\n\r';;
+    *"No mismatch detected for"*)
+      RESULTS_META_SUCCESS="$(($RESULTS_META_SUCCESS+1))"
+    ;;
+    *)
+      RESULTS_META_ERRORS="$((RESULTS_META_ERRORS+1))"
+      RESULTS_ERRORS="${RESULTS_ERRORS}${pkg}"$'\n\r'
+    ;;
   esac
 
 # TEXT
@@ -51,7 +61,7 @@ while IFS= read -r pkg; do
   case $PKG_RESULT in
     *"No mismatch detected for"*) CLASSNAME=good;;
   esac
-  RESULTS_HTML="${RESULTS_HTML}<tr class=\"${CLASSNAME}\"><td>${pkg}</td><td>${PKG_RESULT//\!/!<br>}</td></tr>"
+  RESULTS_HTML_TABLE="${RESULTS_HTML_TABLE}<tr class=\"${CLASSNAME}\"><td>${pkg}</td><td>${PKG_RESULT//\!/!<br>}</td></tr>"
 
 # JSON
   case $PKG_RESULT in
@@ -72,10 +82,12 @@ while IFS= read -r pkg; do
     esac
 done < packages.list
 
-RESULTS_HTML="${RESULTS_HTML}</tbody></table></body></html>"
+RESULTS_HTML="${RESULTS_HTML}<h1>Results of NPM Manifest Check</h1><h2>Overview of results</h2><p>Total: ${RESULTS_META_TOTAL}<br>Successes: ${RESULTS_META_SUCCESS}<br>Errors: ${RESULTS_META_ERRORS}</p>${RESULTS_HTML_TABLE}</tbody></table></body></html>"
 RESULTS_JSON="${RESULTS_JSON%,}]"
 
 echo $RESULTS_ERRORS > "${OUTPUT}-errors.txt"
 echo $RESULTS_TEXT > "${OUTPUT}.txt"
+echo "Total: ${RESULTS_META_TOTAL} - Successes: ${RESULTS_META_SUCCESS} - Errors: $RESULTS_META_ERRORS" > "${OUTPUT}-meta.txt"
+echo "{\"total\": ${RESULTS_META_TOTAL}, \"successes\": ${RESULTS_META_SUCCESS}, \"errors\": $RESULTS_META_ERRORS}" > "${OUTPUT}-meta.json"
 echo $RESULTS_HTML > "${OUTPUT}.html"
 echo $RESULTS_JSON > "${OUTPUT}.json"
